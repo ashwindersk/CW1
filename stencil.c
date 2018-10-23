ff745f97f732a0d564f45cd7b19e70e522ed2266
Ashwinder Khurana <ak16625@my.bristol.ac.uk>
2018-10-23 20:47:06 +0100

    seperating for loop

--- stencil.c
+++ stencil.c
@@ -50,14 +50,44 @@ int main(int argc, char *argv[]) {
 }
 
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
-  for (int i = 0; i < nx; ++i) {
-    for (int j = 0; j < ny; ++j) {
+  //manually amending the values of the corners
+ tmp_image[0]                   = 0.6 * image[0]                  + 0.1*image[1 + ny*0]                  + 0.1*image[0 + ny*1];
+ tmp_image[nx-1 + ny*0]         = 0.6 * image[nx-1 + ny*0]        + 0.1*image[nx-2 + ny*0]               + 0.1*image[nx-1 + ny*1];
+ tmp_image[0 + ny*(nx-1)]       = 0.6 * image[0 + ny*(ny-1)]      + 0.1*image[0 +ny*(ny-2)]              + 0.1*image[(1 + ny*(ny-1))];
+ tmp_image[nx-1 + (ny)*(ny-1)]  = 0.6 * image[nx-1 + (ny)*(ny-1)] + 0.1*image[nx-1 + (ny)*(nx-2)]        + 0.1*image[nx-2 +(nx-1)*(ny)];
+
+
+//top row
+  for(int j = 1; j<nx-1; ++j){
+    tmp_image[j+ny*0] = 0.1*image[j-1 + ny*0] + 0.6*image[j+ny*0]  + 0.1*image[j+1 + ny*0] + 0.1*image[j+ny*1];
+  }
+
+  //first column
+  for(int i = 1; i< ny-1 ; ++i){
+   tmp_image[0+ny*i] = 0.6*image[0+ny*i] + 0.1*image[1+ ny*i] + 0.1*image[0+ny*(i-1)] + 0.1*image[0 + ny*(i+1)];
+  }
+
+//inner image (ny-1)*(nx-1)
+  for (int i = 1; i < ny-1; ++i) {
+    for (int j = 0; j < nx-1; ++j) {
       tmp_image[j+i*ny] = image[j+i*ny] * 0.6;
-      if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.1;
-      if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
-      if (j > 0)    tmp_image[j+i*ny] += image[j-1+i*ny] * 0.1;
-      if (j < ny-1) tmp_image[j+i*ny] += image[j+1+i*ny] * 0.1;
+      tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.1;
+      tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
+      tmp_image[j+i*ny] += image[j-1+i*ny] * 0.1;
+      tmp_image[j+i*ny] += image[j+1+i*ny] * 0.1;
     }
+  }
+
+  //last column
+  for(int i = 1; i< ny-1 ; ++i){
+    int base  = nx-1 + ny*i;
+    tmp_image[base] = 0.6*image[base] + 0.1*image[base-1] + 0.1*image[base -ny] + 0.1*image[base + ny];
+  }
+
+
+  //last row
+  for(int j = 1; j<nx-1; ++j){
+   tmp_image[j + ny*(nx-1)] = 0.6*image[j+ ny*(nx-1)] + 0.1*image[(j-1)+ ny*(nx-1)] + 0.1*image[(j+1)+ ny*(nx-1)] + 0.1*image[j+ ny*(nx-2)];
   }
 }
 
