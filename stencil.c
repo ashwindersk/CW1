2655bd1e24ec789d2ba49a4d4d6f1cd22966ef90
Ashwinder Khurana <ak16625@my.bristol.ac.uk>
2018-10-23 20:17:43 +0100

    for loop change fix

--- stencil.c
+++ stencil.c
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
 }
 
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
-  for (int u = 0; u < nx; ++i) {
+  for (int i = 0; i < nx; ++i) {
     for (int j = 0; j < ny; ++j) {
       tmp_image[j+i*ny] = image[j+i*ny] * 0.6;
       if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.1;
